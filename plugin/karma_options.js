2a1ea59d94ff12a23dfc2d0104a4c0234149420b
Adrian Stainforth <adrian.stainforth@gmail.com>
2016-10-25 16:15:55 +0100

    adding error summery

--- plugin/karma_options.js
+++ plugin/karma_options.js
@@ -28,7 +28,7 @@ var opts = {
 
   , specReporter: {
     maxLogLines: 5 // limit number of lines logged per test
-    , suppressErrorSummary: true  // do not print error summary
+    , suppressErrorSummary: false  // do not print error summary
     , suppressFailed: false  // do not print information about failed tests
     , suppressPassed: false  // do not print information about passed tests
     , suppressSkipped: true  // do not print information about skipped tests
